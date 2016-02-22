4635f30cc678e9e633ea90fcd4e0260bd430b0ed
Dunoo <dany@mygide.uk>
2016-02-22 23:18:07 +0100

    Trocha dizajn multiplayeru [nedokončný]
    Z tej tvojej activity prekopiruj čo robia tie buttony (máš označené
    profesionálnymi poznámkami kam to dať :100: ) :dancer:

--- app/src/main/java/sloth/twotruthsonelie/MainActivity.java
+++ app/src/main/java/sloth/twotruthsonelie/MainActivity.java
@@ -3,14 +3,21 @@ package sloth.twotruthsonelie;
 import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.Display;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 /**
  * Created by Daniel on 1/27/2016.
@@ -18,12 +25,20 @@ import android.widget.ProgressBar;
 
 public class MainActivity extends Activity {
 
-    Button onePhone, twoPhones, settings, wifi, bt;
+    Button onePhone, twoPhones, powerups, settings,
+        wifi, bt, googlePlusMULTI, checkGamesMULTI, playMULTI ;
     boolean is2PhoneOpen = false;
 
     Animation fadeIn;
     Animation fadeInSlower;
     Animation fadeInSlowest;
+    Animation slideUP, slideDOWN;
+    RelativeLayout levelInfo;
+    ImageView logo;
+
+    ProgressBar progressBarPlayMULTI, progressBarCheckMULTI, progressGooglePLus;
+    ProgressBar progressBar, progressBarSett, progressBarMulti, level;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,15 +50,128 @@ public class MainActivity extends Activity {
         settings = (Button) findViewById(R.id.Settings);
         wifi = (Button) findViewById(R.id.WiFi);
         bt = (Button) findViewById(R.id.BT);
+        powerups = (Button) findViewById(R.id.powerups);
+        logo = (ImageView) findViewById(R.id.appNameLogo);
+
+        ////////////////////////Buttony namiesto WIFI → Activita////////////////
+        checkGamesMULTI = (Button) findViewById(R.id.checkGamesMulti);
+        playMULTI = (Button) findViewById(R.id.playMULTI);
+        googlePlusMULTI = (Button) findViewById(R.id.googlePlusMULTI);
 
         fadeIn = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
         fadeInSlower = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in_slower);
         fadeInSlowest = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in_slowest);
+        slideDOWN = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_down);
+        slideUP = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_up);
 
-        final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        final ProgressBar progressBarSett = (ProgressBar) findViewById(R.id.progressBarSett);
-        final ProgressBar progressBarMulti = (ProgressBar) findViewById(R.id.progressBarMulti);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBarSett = (ProgressBar) findViewById(R.id.progressBarSett);
+        progressBarMulti = (ProgressBar) findViewById(R.id.progressBarMulti);
+        level = (ProgressBar) findViewById(R.id.progressBarLevel);
+        progressBarPlayMULTI = (ProgressBar) findViewById(R.id.progressBarPlayMULTI);
+        progressBarCheckMULTI = (ProgressBar) findViewById(R.id.progressCheckMULTI);
+        progressGooglePLus = (ProgressBar) findViewById(R.id.progressGooglePluskMULTI);
 
+        levelInfo = (RelativeLayout) findViewById(R.id.level);
+
+        powerups.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+        playMULTI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this, "watevah", Toast.LENGTH_SHORT).show();
+
+                final ObjectAnimator animation = ObjectAnimator.ofInt(progressBarPlayMULTI, "progress", 0, 500);
+                animation.setDuration(700); //in milliseconds bruv
+                animation.setInterpolator(new DecelerateInterpolator());
+
+                new CountDownTimer(800, 700) {
+
+                    public void onTick(long millisUntilFinished) {
+                        animation.start();
+                    }
+
+                    public void onFinish() {
+
+                        /*
+                        * Tuto
+                        * daj
+                        * chujoviny
+                        * so Spustením hry
+                        * */
+
+                    }
+                }.start();
+
+            }
+
+        });
+
+        checkGamesMULTI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                final ObjectAnimator animation = ObjectAnimator.ofInt(progressBarCheckMULTI, "progress", 0, 500);
+                animation.setDuration(700); //in milliseconds bruv
+                animation.setInterpolator(new DecelerateInterpolator());
+
+                new CountDownTimer(800, 700) {
+
+                    public void onTick(long millisUntilFinished) {
+                        animation.start();
+                    }
+
+                    public void onFinish() {
+
+                        /*
+                        * Tuto
+                        * daj
+                        * chujoviny
+                        * s "CheckGames"
+                        * */
+
+                    }
+                }.start();
+
+            }
+        });
+
+        googlePlusMULTI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final ObjectAnimator animation = ObjectAnimator.ofInt(progressGooglePLus, "progress", 0, 500);
+                animation.setDuration(700); //in milliseconds bruv
+                animation.setInterpolator(new DecelerateInterpolator());
+
+                new CountDownTimer(800, 700) {
+
+                    public void onTick(long millisUntilFinished) {
+                        animation.start();
+                    }
+
+                    public void onFinish() {
+
+                        /*
+                        * Tuto
+                        * daj
+                        * chujoviny
+                        * s GooglePlusom
+                        * */
+
+                    }
+                }.start();
+
+            }
+        });
 
         progressBar.setVisibility(View.VISIBLE);
         progressBar.startAnimation(fadeIn);
@@ -69,19 +197,15 @@ public class MainActivity extends Activity {
                         animation.cancel();
                         progressBar.clearAnimation();
 
-
                         Intent singleplayer = new Intent(MainActivity.this, OnePhone.class);
                         startActivity(singleplayer);
                         overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
-
-
                     }
                 }.start();
 
             }
 
         });
-
 
         progressBarMulti.setVisibility(View.VISIBLE);
         progressBarMulti.setAnimation(fadeIn);
@@ -103,37 +227,38 @@ public class MainActivity extends Activity {
 
                     public void onFinish() {
 
+
+                        checkGamesMULTI.clearAnimation();
+                        playMULTI.clearAnimation();
+                        googlePlusMULTI.clearAnimation();
+                        setSlideDOWN();
+
                         progressBar.setProgress(0);
                         progressBar.clearAnimation();
                         progressBar.setVisibility(View.GONE);
-
 
                         progressBarMulti.setProgress(0);
                         animation.cancel();
                         progressBarMulti.clearAnimation();
                         progressBarMulti.setVisibility(View.GONE);
 
-
                         is2PhoneOpen = true;
 
                         onePhone.clearAnimation();
                         onePhone.setVisibility(View.GONE);
-
                         twoPhones.clearAnimation();
                         twoPhones.setVisibility(View.GONE);
-
                         settings.clearAnimation();
 
-                        wifi.setVisibility(View.VISIBLE);
-                        wifi.setAnimation(fadeIn);
 
-                        bt.setVisibility(View.VISIBLE);
-                        bt.setAnimation(fadeInSlower);
-
-
+//
+//                        wifi.setVisibility(View.VISIBLE);
+//                        wifi.setAnimation(fadeIn);
+//
+//                        bt.setVisibility(View.VISIBLE);
+//                        bt.setAnimation(fadeInSlower);
                     }
                 }.start();
-
             }
         });
 
@@ -168,12 +293,8 @@ public class MainActivity extends Activity {
 
                         progressBarSett.clearAnimation();
                         animation.cancel();
-
-
                     }
                 }.start();
-
-
             }
         });
 
@@ -206,15 +327,121 @@ public class MainActivity extends Activity {
         });
     }
 
+    private void setSlideUP() {
+
+        new CountDownTimer(100, 500) {
+
+            public void onTick(long millisUntilFinished) {
+
+
+            }
+
+            public void onFinish() {
+
+                powerups.setVisibility(View.VISIBLE);
+                powerups.setTranslationY(0);
+                powerups.animate().translationY(200);
+
+                levelInfo.setVisibility(View.VISIBLE);
+                levelInfo.setTranslationY(0);
+                levelInfo.animate().translationY(-600);
+
+                progressBarPlayMULTI.setVisibility(View.GONE);
+                progressBarCheckMULTI.setVisibility(View.GONE);
+                progressGooglePLus.setVisibility(View.GONE);
+            }
+
+        }.start();
+
+    }
+
+    private void setSlideDOWN() {
+
+
+//        new CountDownTimer(100, 500) {
+//
+//            public void onTick(long millisUntilFinished) {
+//            }
+//
+//            public void onFinish() {
+                powerups.setVisibility(View.VISIBLE);
+                powerups.setTranslationY(200);
+                powerups.animate().translationY(0);
+
+                levelInfo.setVisibility(View.VISIBLE);
+                levelInfo.setTranslationY(-600);
+                levelInfo.animate().translationY(0);
+
+                playMULTI.clearAnimation();
+                playMULTI.setVisibility(View.VISIBLE);
+                playMULTI.setAnimation(fadeInSlower);
+
+
+                checkGamesMULTI.clearAnimation();
+                checkGamesMULTI.setVisibility(View.VISIBLE);
+                checkGamesMULTI.setAnimation(fadeInSlower);
+
+                googlePlusMULTI.clearAnimation();
+                googlePlusMULTI.setVisibility(View.VISIBLE);
+                googlePlusMULTI.setAnimation(fadeInSlower);
+
+                progressBarPlayMULTI.setVisibility(View.VISIBLE);
+                progressBarPlayMULTI.setAnimation(fadeInSlower);
+
+                progressBarCheckMULTI.setVisibility(View.VISIBLE);
+                progressBarCheckMULTI.setAnimation(fadeInSlower);
+
+                progressGooglePLus.setVisibility(View.VISIBLE);
+                progressGooglePLus.setAnimation(fadeInSlower);
+            }
+
+//        }.start();
+//    }
+
+
     @Override
     public void onBackPressed() {
 
         if (is2PhoneOpen) {
 
+            logo.setVisibility(View.VISIBLE);
+            setSlideUP();
+
+            playMULTI.clearAnimation();
+            playMULTI.setVisibility(View.GONE);
+            progressBarPlayMULTI.clearAnimation();
+            progressBarPlayMULTI.setVisibility(View.GONE);
+
+            googlePlusMULTI.clearAnimation();
+            googlePlusMULTI.setVisibility(View.GONE);
+            progressGooglePLus.clearAnimation();
+            progressGooglePLus.setVisibility(View.GONE);
+
+            checkGamesMULTI.clearAnimation();
+            checkGamesMULTI .setVisibility(View.GONE);
+            progressBarCheckMULTI.clearAnimation();
+            progressBarCheckMULTI.setVisibility(View.GONE);
+
+            progressBar.setVisibility(View.VISIBLE);
+            progressBar.startAnimation(fadeIn);
+            onePhone.setVisibility(View.VISIBLE);
+            onePhone.startAnimation(fadeIn);
+
+            progressBarMulti.setVisibility(View.VISIBLE);
+            progressBarMulti.setAnimation(fadeIn);
+            twoPhones.setVisibility(View.VISIBLE);
+            twoPhones.startAnimation(fadeIn);
+
+            progressBarSett.setVisibility(View.VISIBLE);
+            progressBarSett.setAnimation(fadeIn);
+            settings.setVisibility(View.VISIBLE);
+            settings.startAnimation(fadeIn);
+
             is2PhoneOpen = false;
-            Intent intent = getIntent();
-            finish();
-            startActivity(intent);
+//            Intent intent = getIntent();
+//            finish();
+//            startActivity(intent);
+
         } else {
             super.onBackPressed();
         }
